e8808b85eb9311a81bdc95cd8b831a13f3caf0d0
andresfeliperm2@gmail.com <andresfeliperm2@gmail.com>
2024-02-01 21:00:47 +0000

    functional deployment

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -11,8 +11,8 @@ resource "aws_ecs_task_definition" "task" {
 
   container_definitions = jsonencode([
     {
-      "name" : "service",
-      "image" : "service:latest",
+      "name" : "nginx",
+      "image" : "public.ecr.aws/nginx/nginx:stable",
       "essential" : true,
       "portMappings" : [
         {
@@ -35,6 +35,13 @@ resource "aws_security_group" "lb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_lb" "lb" {
@@ -51,6 +58,7 @@ resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "8444"
   protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "arn:aws:acm:us-east-1:782823230069:certificate/05ee3dfa-6e0d-41cc-b927-9987b150c021"
 
   default_action {
@@ -70,38 +78,46 @@ resource "aws_security_group" "fargate_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_ecs_service" "service" {
-  name            = "service"
+  name            = "nginx"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
-    security_groups = [aws_security_group.fargate_sg.id]
-    subnets         = ["subnet-0423a1b05695ac070", "subnet-0b9e3ce12a1c5ae9f"]
+    security_groups  = [aws_security_group.fargate_sg.id]
+    subnets          = ["subnet-0423a1b05695ac070", "subnet-0b9e3ce12a1c5ae9f"]
+    assign_public_ip = true
   }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.group.arn
-    container_name   = "service"
+    container_name   = "nginx"
     container_port   = 8080
   }
 }
 
 resource "aws_lb_target_group" "group" {
-  name     = "my-targets"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-05bb20e5204f2c6b4"
+  name        = "web-ui"
+  port        = 8080
+  protocol    = "TCP"
+  vpc_id      = "vpc-05bb20e5204f2c6b4"
+  target_type = "ip"
 
   health_check {
     enabled             = true
     interval            = 30
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
+    port                = "8080"
+    protocol            = "TCP"
     timeout             = 3
     healthy_threshold   = 3
     unhealthy_threshold = 3
